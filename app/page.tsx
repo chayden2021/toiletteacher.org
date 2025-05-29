46d144baadae39b5634e796abe6f5ab4423cb919
chayden21 <haydjc21@wfu.edu>
2025-05-29 15:16:14 -0500

    lets see

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function HomePage() {
 
         {/* Main Content */}
         <main className="mb-12">
-          <p className="text-gray-800 leading-relaxed text-lg mb-8">
+          <p className="text-center text-gray-800 leading-relaxed text-lg mb-8">
             We spend a lot of time on the toilet. What if we used that time to get closer to the Truth? Choose a toilet teacher below and take a few deep breaths with their daily message. Feel free to bookmark their page. Enjoy!
           </p>
 
