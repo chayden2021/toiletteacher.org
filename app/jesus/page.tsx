391a0af8159a450c067917d95203a6cc5ad03f56
chayden21 <haydjc21@wfu.edu>
2025-05-29 14:52:21 -0500

    made jesus title bigger

--- app/jesus/page.tsx
+++ app/jesus/page.tsx
@@ -17,8 +17,8 @@ export default function JesusPage() {
               ToiletTeacher.org
             </Link>
           </h1>
-          <p className="text-lg text-gray-600 italic mb-4">Jesus</p>
-          <p className="text-sm text-gray-500">{currentDate}</p>
+          <p className="text-2xl text-gray-600 italic mb-4">Jesus</p>
+          <p className="text-lg text-gray-500">{currentDate}</p>
         </header>
 
         {/* Main Content */}
