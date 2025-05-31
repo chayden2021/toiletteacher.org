0ed0b6294a69503140b53d15970d606898850b63
chayden21 <haydjc21@wfu.edu>
2025-05-30 22:38:57 -0500

    jesus quotes are powrful

--- lib/jesus-quotes.ts
+++ lib/jesus-quotes.ts
@@ -1,3 +1,17 @@
 export const jesusQuotes = [
-  '"A new command I give you: Love one another. As I have loved you, so you must love one another." - John 13:34'
+  '"A new command I give you: Love one another. As I have loved you, so you must love one another." - John 13:34',
+  '“Truly I tell you, unless you change and become like little children, you will never enter the kingdom of heaven. Therefore, whoever takes the lowly position of this child is the greatest in the kingdom of heaven. And whoever welcomes one such child in my name welcomes me." - Matthew 18:3-5',
+  '“Do not store up for yourselves treasures on earth, where moths and vermin destroy, and where thieves break in and steal. But store up for yourselves treasures in heaven, where moths and vermin do not destroy, and where thieves do not break in and steal. For where your treasure is, there your heart will be also." - Matthew 6:19-21',
+  '"For those who exalt themselves will be humbled, and those who humble themselves will be exalted." - Matthew 23:12',
+  '"Therefore I tell you, whatever you ask for in prayer, believe that you have received it, and it will be yours." - Mark 11:24' ,
+  '"And when you stand praying, if you hold anything against anyone, forgive them, so that your Father in heaven may forgive you your sins.” - Mark 11:25',
+  '"do not worry about tomorrow, for tomorrow will worry about itself. Each day has enough trouble of its own." - Matthew 6:34',
+  '"So do not worry, saying, ‘What shall we eat?’ or ‘What shall we drink?’ or ‘What shall we wear?’ For the pagans run after all these things, and your heavenly Father knows that you need them. But seek first his kingdom and his righteousness, and all these things will be given to you as well." - Matthew 6:31-33',
+  '“What do you think? If a man owns a hundred sheep, and one of them wanders away, will he not leave the ninety-nine on the hills and go to look for the one that wandered off? And if he finds it, truly I tell you, he is happier about that one sheep than about the ninety-nine that did not wander off. In the same way your Father in heaven is not willing that any of these little ones should perish.” - Matthew 18:12-14',
+  '“Then he called the crowd to him along with his disciples and said: “Whoever wants to be my disciple must deny themselves and take up their cross and follow me. For whoever wants to save their lifewill lose it, but whoever loses their life for me and for the gospel will save it. What good is it for someone to gain the whole world, yet forfeit their soul?” - Mark 8:34-36',
+  '“You have heard that it was said, ‘Love your neighbor  and hate your enemy. But I tell you, love your enemies and pray for those who persecute you.” - Matthew 5:43-44',
+  '“So in everything, do to others what you would have them do to you, for this sums up the Law and the Prophets.” - Matthew 7:12',
+  '“Love the Lord your God with all your heart and with all your soul and with all your mind. This is the first and greatest commandment. And the second is like it: Love your neighbor as yourself. All the Law and the Prophets hang on these two commandments.” - Matthew 22:37-40',
+  '“I have told you these things, so that in me you may have peace. In this world you will have trouble. But take heart! I have overcome the world.” - John 16:33',
+  '"So I say to you: Ask and it will be given to you; seek and you will find; knock and the door will be opened to you." - Luke 11:9-10',
 ]
